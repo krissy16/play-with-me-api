bda5e88ff38a8b79211409ff00a28dba7137c113
Krissy <krissysaenz@gmail.com>
2020-06-28 20:48:54 -0700

    deployment settings fixed

--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
-const express = require('express');
- const app = express();
+const app = require('./app')
+const { PORT } = require('./config')
 
- const PORT = process.env.PORT || 3000;
-
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
-
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
- module.exports = {app};
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
