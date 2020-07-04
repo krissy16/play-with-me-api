87a831ddf149be949c2d960cbe3220e2c97c6d91
Krissy <krissysaenz@gmail.com>
2020-07-03 17:35:08 -0700

    cors fixed

--- src/app.js
+++ src/app.js
@@ -16,8 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
-//app.use( cors({ origin: CLIENT_ORIGIN }));
+app.use( cors({ origin: CLIENT_ORIGIN }));
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://playwithme_admin@localhost/playwithme',
-    CLIENT_ORIGIN: 'https://play-with-me.vercel.app/',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   }
