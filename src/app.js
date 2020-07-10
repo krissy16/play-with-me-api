54de1d432a7cebb152c9e658c2b3fab8cee0022a
Krissy <krissysaenz@gmail.com>
2020-07-10 01:13:10 -0700

    cors open

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.unsubscribe(cors())
+app.use(cors())
 //app.use( cors({ origin: CLIENT_ORIGIN }));
 
 app.use(function validateBearerToken(req, res, next) {
