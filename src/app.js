235ae39c7d5ad98bea5e9c4eb5a8bfc31f1077f3
Krissy <krissysaenz@gmail.com>
2020-07-10 19:59:30 -0700

    comments added

--- src/app.js
+++ src/app.js
@@ -16,9 +16,10 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-//app.use(cors())
+//restrict access to only the client origin
 app.use( cors({ origin: CLIENT_ORIGIN }));
 
+//validate the api token
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
@@ -27,7 +28,6 @@ app.use(function validateBearerToken(req, res, next) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-  // move to the next middleware
   next()
 })
 
